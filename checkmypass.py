ebd15895bc277b76d47fc705cade4dfd0572e681
Zachariah Chow <zachariahchow@gmail.com>
2020-08-21 16:42:40 +0800

    feat: Add API request function

---
+++ checkmypass.py
@@ -0,0 +1,9 @@
+import requests
+
+
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching: {res.status_code}, check API and run again ')
+    print(res)
