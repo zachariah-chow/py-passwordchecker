c35d0a99a340587ae595a5ca02ca9486f4021060
Zachariah Chow <zachariahchow@gmail.com>
2020-08-21 16:50:45 +0800

    feat: Add SHA1 API checker function

--- checkmypass.py
+++ checkmypass.py
@@ -1,4 +1,5 @@
 import requests
+import hashlib
 
 
 def request_api_data(query_char):
@@ -6,4 +7,12 @@ def request_api_data(query_char):
     res = requests.get(url)
     if res.status_code != 200:
         raise RuntimeError(f'Error fetching: {res.status_code}, check API and run again ')
-    print(res)
+    return res
+
+
+def pwned_api_check(password):
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)
+    print(first5_char, tail)
+    return response
