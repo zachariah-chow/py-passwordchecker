d0beddc46dccc35fcbfad51d6d8a7c712803b411
Zachariah Chow <zachariahchow@gmail.com>
2020-08-21 17:56:54 +0800

    feat: Add function for CLI args

--- checkmypass.py
+++ checkmypass.py
@@ -1,5 +1,6 @@
 import requests
 import hashlib
+import sys
 
 
 def request_api_data(query_char):
@@ -13,7 +14,9 @@ def request_api_data(query_char):
 def get_password_leaks_count(hashes, hash_to_check):
     hashes = (line.split(':') for line in hashes.text.splitlines())
     for h, count in hashes:
-        print(h, count)
+        if h == hash_to_check:
+            return count
+    return 0
 
 
 def pwned_api_check(password):
@@ -24,4 +27,14 @@ def pwned_api_check(password):
     return get_password_leaks_count(response, tail)
 
 
-print(pwned_api_check('123'))
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(f'{password} was found {count} times. ')
+        else:
+            print(f'{password} was NOT found.')
+    return 'done'
+
+
+main(sys.argv[1:])
